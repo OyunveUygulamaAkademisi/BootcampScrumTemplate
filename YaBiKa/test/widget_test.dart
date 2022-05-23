a7ce970e0edbf056b8e73fc267e3150ba8a27efa
Umutcan Ceyhan <umutcan.ceyhan@epigra.com>
2022-05-23 19:46:50 +0300

    Static merge branch

--- YaBiKa/test/widget_test.dart
+++ YaBiKa/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:yabika/main.dart';
+import 'package:yabika/mainPage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
